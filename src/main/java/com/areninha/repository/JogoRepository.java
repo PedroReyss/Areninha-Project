f12a266392b2798930c160b22da2332deff3f488
Pedro Henrique Simões Reys <reys.pedro1@gmail.com>
2025-11-22 23:06:04 -0300

    Delete src/main directory

--- src/main/java/com/areninha/repository/JogoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.areninha.repository;
-
-import com.areninha.entity.Jogo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface JogoRepository extends JpaRepository<Jogo, Long> {
-    List<Jogo> findByNomeContainingIgnoreCase(String nome);
-}
